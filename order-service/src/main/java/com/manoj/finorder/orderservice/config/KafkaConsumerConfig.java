a0de5f1f947fcec8ab9beb1353fb30fcc7a4a467
manojk-sai <manojsaikothapati@gmail.com>
2026-01-15 13:21:13 -0600

    Kafka config

--- order-service/src/main/java/com/manoj/finorder/orderservice/config/KafkaConsumerConfig.java
+++ order-service/src/main/java/com/manoj/finorder/orderservice/config/KafkaConsumerConfig.java
@@ -12,6 +12,8 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -26,8 +28,11 @@ public class KafkaConsumerConfig {
     @Bean
     public ConsumerFactory<String, InventoryEvent> inventoryConsumerFactory() {
         Map<String, Object> props = baseConsumerProps();
-        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, InventoryEvent.class);
-        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(InventoryEvent.class, false));
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.manoj.finorder.orderservice.event.InventoryEvent");
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        return new DefaultKafkaConsumerFactory<>(props,
+                new ErrorHandlingDeserializer<>(new StringDeserializer()),
+                new ErrorHandlingDeserializer<>(new JsonDeserializer<>(InventoryEvent.class, false)));
     }
 
     @Bean
@@ -41,13 +46,16 @@ public class KafkaConsumerConfig {
     public ConsumerFactory<String, PaymentEvent> paymentConsumerFactory() {
         Map<String, Object> props = baseConsumerProps();
         props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, PaymentEvent.class);
-        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(PaymentEvent.class, false));
+        return new DefaultKafkaConsumerFactory<>(props,
+                new ErrorHandlingDeserializer<>(new StringDeserializer()),
+                new ErrorHandlingDeserializer<>(new JsonDeserializer<>(PaymentEvent.class, false)));
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, InventoryEvent> inventoryKafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, InventoryEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(inventoryConsumerFactory());
+        factory.setCommonErrorHandler(new DefaultErrorHandler());
         return factory;
     }
 
@@ -55,8 +63,17 @@ public class KafkaConsumerConfig {
     public ConcurrentKafkaListenerContainerFactory<String, PaymentEvent> paymentKafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, PaymentEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(paymentConsumerFactory());
+        factory.setCommonErrorHandler(new DefaultErrorHandler());
         return factory;
     }
+    @Bean
+    public JsonDeserializer<InventoryEvent> jsonDeserializer() {
+        JsonDeserializer<InventoryEvent> deserializer = new JsonDeserializer<>(InventoryEvent.class);
+        deserializer.setRemoveTypeHeaders(false);
+        deserializer.addTrustedPackages("*");
+        return deserializer;
+    }
+
     private Map<String, Object> baseConsumerProps() {
         Map<String, Object> props = new HashMap<>(kafkaProperties.buildConsumerProperties());
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
--- order-service/src/main/java/com/manoj/finorder/orderservice/config/KafkaProducerConfig.java
+++ order-service/src/main/java/com/manoj/finorder/orderservice/config/KafkaProducerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,7 +26,8 @@ public class KafkaProducerConfig {
     public KafkaTemplate<String, InventoryEvent> inventoryKafkaTemplate(KafkaProperties kafkaProperties) {
         Map<String, Object> props = new HashMap<>(kafkaProperties.buildProducerProperties());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
         return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(props));
     }
 }
